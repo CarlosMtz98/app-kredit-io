d42efd381192ea5e47c43bba11fd8ccab63b74e3
Jordan <30836115+duncanista@users.noreply.github.com>
2019-04-28 09:14:16 -0500

    UPDATE

--- src/containers/Solictud/form.js
+++ src/containers/Solictud/form.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import * as typeformEmbed from '@typeform/embed';
-import './formStyles.css';
-=======
 import React from "react";
+import './formStyles.css';
 import { Button, Row, Col, FormGroup, FormControl, FormLabel, Container } from "react-bootstrap";
->>>>>>> 3fb6032e1688b753acd5860bcdb3e4cd58b6fc09
 
 export  class Form extends React.Component {
   constructor(props) {
@@ -48,83 +43,13 @@ export  class Form extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
-        <div className="kredit-form">
-        
-            <div className="container kredit-req">
-            <form class=" p-5">
-
-                <p class="h4 mb-4 text-left kredit-title">{ props.title }</p>
-                <div class="form-row">
-                    <div class="col-md-5 mb-3">
-                        <label for="validationServer01">Puntaje SAT</label>
-                        <input type="text" class="form-control" id="validationServer01" placeholder="ej. 83" required/>
-                    </div>
-                    <div className="col-md-2"></div>
-                    <div class="col-md-5 mb-3">
-                        <label for="validationServer02">Puntaje Buró de Crédito</label>
-                        <input type="text" class="form-control" id="validationServer02" placeholder="ej. 78"required/>
-                    </div>
-                </div>
-                <div className="form-row">
-                    <div class="col-md-5 mb-3">
-                            <label for="validationServerUsername">Ingreso Mensual</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="inputGroupPrepend3">
-                                        <i class="fas fa-dollar-sign"></i>
-                                    </span>
-                                </div>
-                                <input type="text" class="form-control" id="validationServerUsername" placeholder="ej. 50,000" aria-describedby="inputGroupPrepend3" required />
-                            </div>
-                        </div>
-                        <div className="col-md-2"></div>
-                        <div class="col-md-5 mb-3">
-                        <label for="validationServerUsername">Monto del Crédito</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupPrepend3">
-                                    <i class="fas fa-dollar-sign"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" id="validationServerUsername" placeholder="ej. 50,000" aria-describedby="inputGroupPrepend3" required />
-                        </div>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col-md-5 mb-3">
-                        <label for="validationServerUsername">Propuesta de Pagos en Meses</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupPrepend3">
-                                    <i class="fas fa-calendar-day"></i>
-                                </span>
-                            </div>
-                            <input type="text" class="form-control" id="validationServerUsername" placeholder="ej. 12" aria-describedby="inputGroupPrepend3" required />
-                        </div>
-                    </div>
-                    
-                </div>
-                <div class="form-group">
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="invalidCheck3" required/>
-                    <label class="form-check-label" for="invalidCheck3">
-                        Acepto los términos y condiciones
-                    </label>
-                    </div>
-                </div>
-                    <button class="btn btn-primary" type="submit">Solicitar</button>
-                </form>
-            </div>
-        </div>
-=======
-      <div className="SignUp">
+      <div className="SignUp kredit-form">
         
         <Container>
-            <Row className="justify-content-md-center">
+            <Row className="justify-content-md-center kredit-req">
                 <Col md={8}>
                     <form onSubmit={this.handleSubmit}>
-                    <h4>Ingresa tus datos y te contestaremos lo más rponto posible</h4>
+                    <h4 class="kredit-title">Ingresa tus datos y te contestaremos lo más pronto posible.</h4>
                     <Row>
                         <Col>
                         <FormGroup controlId="puntosSat" bsSize="large">
@@ -195,7 +120,6 @@ export  class Form extends React.Component {
             </Row>
         </Container>
       </div>
->>>>>>> 3fb6032e1688b753acd5860bcdb3e4cd58b6fc09
     );
   }
 }
--- src/containers/Solictud/index.js
+++ src/containers/Solictud/index.js
@@ -5,13 +5,8 @@ class Solicitud extends React.Component {
     
     render() {
         return (
-<<<<<<< HEAD
-            <div className="">
-                <RequestForm title="Nueva solicitud de crédito"/>
-=======
             <div>
                 <RequestForm title="Nueva solicitud de Credito"/>
->>>>>>> 2905b9ed197895af34ce037fd33595ea144dd9c6
             </div> 
         );
     }
